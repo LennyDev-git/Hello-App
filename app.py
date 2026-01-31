35e7c5af759e81eb97c6f4aea123722d11f2a4c4
LennyDev-git <fisbecklennold@gmail.com>
2026-01-31 11:24:15 +0100

    Added a error when the name is nothing

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ root.title("Hello App")
 
 def hello():
     name = entry.get()
-    messagebox.showinfo("Hello",f"Hello {name} have a good day!")
+    
+    if name == "":
+        messagebox.showerror("ERROR","Please enter a name")
+        
+    else:
+        messagebox.showinfo("Hello",f"Hello {name} have a nice day!")
     
 entry = tk.Entry(root)
 entry.pack()
 
 button = tk.Button(root,text="Say hello",command=hello)
 button.pack()
+
+
 root.mainloop()
